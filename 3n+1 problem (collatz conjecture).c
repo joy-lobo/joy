24378c64b0908de67afdb929f96d4efcdd3f09b4
joy-lobo <104333452+joy-lobo@users.noreply.github.com>
2023-04-02 01:01:22 +0600

    Add files via upload

---
+++ 3n+1 problem (collatz conjecture).c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int m,n,p,q[],i,j,k=0,x=0;
+
+int main()
+{
+    printf("Input the range:\n");
+    scanf("%d%d",&m,&n);
+
+    for(i=m; i<=n; i++)
+    {
+        q[i]=0;
+        steps(i);
+        printf("\nNumber = %d , steps = %d",i,q[i]);
+    }
+    for(i=m; i<=n; i++)
+    {
+        if(q[i]>=k)
+        {
+            k=q[i];
+            x=i;
+        }
+    }
+    printf("\nBetween '%d' and '%d', the number '%d' has the highest life cycle of '%d' steps..",m,n,x,k);
+}
+
+int steps(int n)
+{
+    if (n%2==0 && n!=1)
+    {
+        p=n/2;
+        n=p;
+        q[i]++;
+        steps(n);
+    }
+    else if (n%2!=0 && n!=1)
+    {
+        p=(3*n)+1;
+        n=p;
+        q[i]++;
+        steps(n);
+    }
+    return q;
+}
